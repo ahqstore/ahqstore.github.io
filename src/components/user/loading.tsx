417bf8b7f1dbbe31540fb37bb00395dc5f5c083c
AHQ <ahqsecret@gmail.com>
2024-05-16 14:45:10 +0530

    Update loading.tsx

--- src/components/user/loading.tsx
+++ src/components/user/loading.tsx
@@ -2,7 +2,7 @@ export default function Loading() {
   return (
     <div className="tw-flex tw-flex-col">
       <img className="user-loading" src="/loading.svg" />
-      <h6 className="tw-mx-auto" style={{ fontWeight: "50" }}>Thanks for loading.io for the animation</h6>
+      <h6 className="tw-mx-auto" style={{ fontWeight: "50" }}>Thanks to loading.io for the animation</h6>
     </div>
   )
 }
